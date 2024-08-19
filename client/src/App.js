58ae5a0f14f8d368c16b9d4651102b5986e6dcd2
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-08-19 22:06:02 +0530

    creating home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
+import React from 'react'
+import styled, { ThemeProvider } from "styled-components"
+import { darkTheme } from './utils/Theme'
+
+const Container =styled.div`
+  width:100%;
+  height:100%;
+  display:flex;
+  background: ${({theme}) => theme.bg};
+`
+
+const Wrapper =  styled.div`
+  height:100%;
+  width:100%;
+  display:flex;
+  flex-direction:column;
+  
+`;
 
 function App() {
   return (
-    <div className="App">
-
-    <div>hey hello how is it going</div>
-    how you doing 
-    </div>
-  );
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <Wrapper>
+          Hello
+        </Wrapper>
+      </Container>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
